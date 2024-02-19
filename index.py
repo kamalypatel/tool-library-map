7f2c78e8a4dbf1c092ccec1346a790aa5590ba31
Kamal Patel <80398694+kamalypatel@users.noreply.github.com>
2024-02-19 11:35:51 -0500

    added pandas for reading csv of value pairs

--- index.py
+++ index.py
@@ -1,13 +1,13 @@
 import folium
 from folium.plugins import HeatMap
+import pandas as pd
 
-data = [
-    [40.748817, -73.985428],
-    [37.774929, -122.419416],
-]
+data = pd.read_csv('your_file.csv', usecols=['Lat', 'Long'])
+
+lat_lon = data[['Lat', 'Long']].values.tolist()
 
 m = folium.Map(location=[42.887691, -78.879372], zoom_start=10)
 
-HeatMap(data).add_to(m)
+HeatMap(lat_lon).add_to(m)
 
 m.save('heatmap.html')
